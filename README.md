aea592333f571aff9e0f5f4db2cba14184c1aa73
Xavier López <xavier.lopeze@gmail.com>
2021-05-16 20:11:49 +0200

    get_flat_data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Librebor-insolvencias
+
+Analysis de datos de LibreBOR para analizar los datos de insolvencias para ver cómo ha afectado el covid19 al tejido empresarial.
+
+## Objetivos:
+
+- Visualizar insolvencias mensual interanual
+- Visualizar involvencias por provincias
+- Visual insolvencias por sector
---
+++ get_flat_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import ast
+
+filename = "data/insolvencias_2021.04.20"
+json_df = pd.read_json(filename + '.json', encoding='UTF-8')
+df = json_df
+
+df_i = df.insolvencias.apply(pd.Series)
+df_i["Company"] = [ast.literal_eval(str(x).replace("[","").replace("]","")) for x in df_i.company]
+df_i_company = df_i.join(df_i.Company.apply(pd.Series))
+
+df_i_c = df_i_company
+df_i_c["Announcements"] = [ast.literal_eval(str(x).replace("[","").replace("]","")) for x in df_i_c.announcements]
+df_i_c.Announcements.apply(pd.Series)
+df_full = df_i_c.join(df_i_c.Announcements.apply(pd.Series))
+
+df_full.to_csv(filename + '.csv')
